2d73dabc021429fe1b7acac03dfe759ef1402a3c
Raj/aryan <raj.aryan2518@gmail.com>
2026-03-15 13:12:48 +0530

    authentication page added

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
             />
             Deploy now
           </a>
-          <button class="btn btn-warning">Warning</button>
+          <button className="btn btn-warning">Warning</button>
 
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
